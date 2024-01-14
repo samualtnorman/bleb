f4cacc0f215480d659630aaf56d82674fa074fe2
Samual <me@samual.uk>
2024-01-14 19:48:11 +0000

    update

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Better LEB128
 bleb
 
-[![Build](https://github.com/samualtnorman/bleb/actions/workflows/build.yml/badge.svg)](https://github.com/samualtnorman/bleb/actions/workflows/build.yml) [![Lint](https://github.com/samualtnorman/bleb/actions/workflows/lint.yml/badge.svg)](https://github.com/samualtnorman/bleb/actions/workflows/lint.yml)
+[![Lint](https://github.com/samualtnorman/bleb/actions/workflows/lint.yml/badge.svg)](https://github.com/samualtnorman/bleb/actions/workflows/lint.yml) [![Publish](https://github.com/samualtnorman/bleb/actions/workflows/publish.yml/badge.svg)](https://github.com/samualtnorman/bleb/actions/workflows/publish.yml)
 
-| Range           | bleb    | LEB128  |
-|-----------------|---------|---------|
-| 0 - 127         | 1 byte  | 1 byte  |
-| 128 - 16383     | 2 bytes | 2 bytes |
-| 16384 - 16511   | 2 bytes | 3 bytes |
-| 16512 - 2097151 | 3 bytes | 3 bytes |
+| Range             | bleb    | LEB128  |
+|-------------------|---------|---------|
+| 0 - 127           | 1 byte  | 1 byte  |
+| 128 - 16383       | 2 bytes | 2 bytes |
+| 16384 - 16511     | 2 bytes | 3 bytes |
+| 16512 - 2097151   | 3 bytes | 3 bytes |
 | 2097152 - 2113663 | 3 bytes | 4 bytes |
 | ... |
--- rollup.config.js
+++ rollup.config.js
@@ -1,102 +1,48 @@
-import * as t from "@babel/types"
-import alias from "@rollup/plugin-alias"
+import babelPresetEnv from "@babel/preset-env"
+import babelPresetTypescript from "@babel/preset-typescript"
 import { babel } from "@rollup/plugin-babel"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import terser from "@rollup/plugin-terser"
 import { findFiles } from "@samual/lib/findFiles"
-import MagicString from "magic-string"
-import * as Path from "path"
-import { relative as getRelativeFilePath } from "path"
-import packageConfig from "./package.json" assert { type: "json" }
+import babelPluginHere from "babel-plugin-here"
+import { readFile } from "fs/promises"
 
-const SourceFolder = "src"
-const Minify = true
+const SOURCE_FOLDER = "src"
+const MINIFY = true
 
-const externalDependencies = []
+/** @type {() => Promise<import("rollup").RollupOptions>} */
+export default async () => {
+	const [ foundFiles, packageJsonString ] =
+		await Promise.all([ findFiles(SOURCE_FOLDER), readFile("package.json", { encoding: "utf8" }) ])
 
-if ("dependencies" in packageConfig)
-	externalDependencies.push(...Object.keys(packageConfig.dependencies))
+	const packageJson = JSON.parse(packageJsonString)
 
-if ("optionalDependencies" in packageConfig)
-	externalDependencies.push(...Object.keys(packageConfig.optionalDependencies))
+	const externalDependencies = [
+		..."dependencies" in packageJson ? Object.keys(packageJson.dependencies) : [],
+		..."optionalDependencies" in packageJson ? Object.keys(packageJson.optionalDependencies) : []
+	]
 
-export default findFiles(SourceFolder).then(foundFiles => /** @type {import("rollup").RollupOptions} */ ({
-	input: Object.fromEntries(
-		foundFiles
-			.filter(path => path.endsWith(".ts") && !path.endsWith(".d.ts"))
-			.map(path => [ path.slice(SourceFolder.length + 1, -3), path ])
-	),
-	output: { dir: "dist", chunkFileNames: "[name]-.js", generatedCode: "es2015", interop: "auto", compact: Minify },
-	plugins: [
-		babel({
-			babelHelpers: "bundled",
-			extensions: [ ".ts" ],
-			presets: [
-				[ "@babel/preset-env", { targets: { node: "14" } } ],
-				[ "@babel/preset-typescript", { allowDeclareFields: true } ]
-			],
-			plugins: [
-				{
-					name: "babel-plugin-here",
-					visitor: {
-						Program(path) {
-							if (!path.scope.hasGlobal("HERE"))
-								return
-
-							const [ variableDeclarationPath ] = path.unshiftContainer(
-								"body",
-								t.variableDeclaration("let", [ t.variableDeclarator(t.identifier("HERE")) ])
-							)
-
-							path.scope.crawl()
-
-							const filePath = getRelativeFilePath(".", this.file.opts.filename)
-
-							for (const referencePath of path.scope.getBinding("HERE").referencePaths) {
-								const line = referencePath.node.loc.start.line
-								const column = referencePath.node.loc.start.column + 1
-
-								if (referencePath.parent.type != "TemplateLiteral") {
-									referencePath.replaceWith(t.stringLiteral(`${filePath}:${line}:${column}`))
-
-									continue
-								}
-
-								const { parent, node } = referencePath
-								const index = parent.expressions.indexOf(/** @type {any} */ (node))
-								const quasi = parent.quasis[index].value.raw
-								const nextQuasi = parent.quasis[index + 1].value.raw
-
-								parent.expressions.splice(index, 1)
-								delete parent.quasis[index].value.cooked
-								parent.quasis[index].value.raw = `${quasi}${filePath}:${line}:${column}${nextQuasi}`
-								parent.quasis[index].tail = parent.quasis[index + 1].tail
-								parent.quasis.splice(index + 1, 1)
-							}
-
-							variableDeclarationPath.remove()
-						}
-					}
-				}
-			]
-		}),
-		nodeResolve({ extensions: [ ".ts" ] }),
-		Minify && terser({ keep_classnames: true, keep_fnames: true }),
-		{
-			name: "rollup-plugin-shebang",
-			renderChunk(code, { fileName }) {
-				if (!fileName.startsWith("bin/"))
-					return undefined
-
-				const magicString = new MagicString(code).prepend("#!/usr/bin/env node\n")
-
-				return { code: magicString.toString(), map: magicString.generateMap({ hires: true }) }
-			}
-		},
-		alias({ entries: [ { find: /^\//, replacement: `${Path.resolve(SourceFolder)}/` } ] })
-	],
-	external:
-		source => externalDependencies.some(dependency => source == dependency || source.startsWith(`${dependency}/`)),
-	preserveEntrySignatures: "allow-extension",
-	strictDeprecations: true
-}))
+	return {
+		input: Object.fromEntries(
+			foundFiles.filter(path => path.endsWith(".ts") && !path.endsWith(".d.ts"))
+				.map(path => [ path.slice(SOURCE_FOLDER.length + 1, -3), path ])
+		),
+		output: { dir: "dist", generatedCode: "es2015", compact: MINIFY },
+		plugins: [
+			babel({
+				babelHelpers: "bundled",
+				extensions: [ ".ts" ],
+				presets: [
+					[ babelPresetEnv, { targets: { node: "20" } } ],
+					[ babelPresetTypescript, { allowDeclareFields: true } ]
+				],
+				plugins: [ babelPluginHere() ]
+			}),
+			nodeResolve({ extensions: [ ".ts" ] }),
+			MINIFY && terser({ keep_classnames: true, keep_fnames: true })
+		],
+		external: source =>
+			externalDependencies.some(dependency => source == dependency || source.startsWith(`${dependency}/`)),
+		strictDeprecations: true
+	}
+}
